1286db3d56090817797ca6d61b6403b628247780
Gloriousjoy <gloriousfab99@gmail.com>
2022-07-06 01:24:46 -0700

    correct indent error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
- /**
-  * main - that prints "_putchar" followed by new line
-  *
-  * Return: Always 0.
-  **/
+/**
+ * main - that prints "_putchar" followed by new line
+ * Return: Always 0.
+ */
 int main(void)
 {
 	_putchar('_');
