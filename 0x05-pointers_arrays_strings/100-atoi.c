a2b5afd7f83e70992ee3d8368286aa5d1ce88695
Gloriousjoy <gloriousfab99@gmail.com>
2022-07-12 07:45:34 -0700

    converts string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _atoi - converts a string into an integer.
+ * @s: string to be conerted.
+ * Return: the integer alue of the conerted string.
+ **/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
